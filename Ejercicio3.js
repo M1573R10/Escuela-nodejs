4c9766ea8c8c3295e7212172edea0ed3b5978584
Kevin Medina <96708929+M1573R10@users.noreply.github.com>
2022-11-03 16:39:14 -0500

    Add files via upload

---
+++ Ejercicio3.js
@@ -0,0 +1,5 @@
+var SumaCuadrado = function (x, y) {
+    var resultado = (x + y) ^ 2;
+    return console.log('El resutado es ' + resultado);
+};
+SumaCuadrado(2, 4);
---
+++ Ejercicio3.ts
@@ -0,0 +1,6 @@
+let SumaCuadrado=(x: number , y: number) =>{
+    const resultado = (x+y)^2;
+    return console.log('El resutado es ' + resultado);
+}
+
+SumaCuadrado(2,4);
